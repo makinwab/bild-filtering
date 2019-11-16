4ff85f63ee96309dab04407333bc35e0fc53f28c
makinwab <makinwa37@gmail.com>
2019-11-16 13:08:50 +0100

    Complete c2 project

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Router, Response, Request } from 'express'
+import { filterImageFromURL, deleteLocalFiles, isValidURL } from './util/util';
+import path from 'path';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async (req, res) => {
+    const image_url = req.query.image_url;
+
+    if (image_url && isValidURL(image_url)) {
+      const filteredImage: string = await filterImageFromURL(image_url)
+
+      res.status(200).sendFile(filteredImage, async (err) => {
+        if (err) console.error(err)
+        await deleteLocalFiles([filteredImage]);
+      });
+    } else {
+      res.sendStatus(400);
+    }
+  })
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
